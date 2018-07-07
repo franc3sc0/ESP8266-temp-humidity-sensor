f04c53cdef505f8da59c5122a3929af86a189f21
Francesco Vass <francesco.vass@gmail.com>
2018-07-07 09:26:58 +0200

    Add handling of Wifi events Use LEDs to monitor status

--- src/monitor/monitor.ino
+++ src/monitor/monitor.ino
@@ -6,10 +6,10 @@
 #define DHTPIN 4      // DHT Sensor connected to digital pin 2.
 #define DHTTYPE DHT11 // Type of DHT sensor.
 
-const unsigned long postingInterval = 60L * 1000L; // Post data every 60 seconds.
-
 WiFiClient client;        // Initialize the Wifi client library.
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor.
+
+const unsigned long postingInterval = 60L * 1000L; // Post data interval (60 seconds)
 
 int connectWifi();
 void updateDHT();
@@ -17,14 +17,59 @@ unsigned long lastConnectionTime = 0;
 float dhtTemp = 0;
 float dhtHumidity = 0;
 
+WiFiEventHandler gotIpEventHandler, disconnectedEventHandler;
+
+// Attempts a connection to WiFi network and repeats until successful.
+int connectWifi()
+{
+  digitalWrite(LED_BUILTIN, LOW);  // BUILTIN LED on to indicate WiFi connection in progress
+  WiFi.begin(ssid, wifiPwd);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println( "Connecting to WiFi..." );
+    delay(2500);
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+  // Serial.println( "Connected to WiFi" );
+}
+
+void updateDHT()
+// Get the most recent readings for temperature and humidity.
+{
+  digitalWrite(2, LOW);   // ESP-12 LED on to indicate we're reading temp, humidity
+  // To return Fahrenheit use dht.readTemperature(true)
+  dhtTemp = dht.readTemperature();
+  dhtHumidity = dht.readHumidity();
+  Serial.println( dhtTemp );
+  Serial.println( dhtHumidity );
+  digitalWrite(2, HIGH);
+}
+
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("Start");
+  Serial.begin( 115200 );
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(2, OUTPUT);               // Initialize GPIO2 pin as an output
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(2, HIGH);
+  Serial.println( "Starting" );
+
+  gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP & event)
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn  BUILTIN LED off to indicate WiFi connection
+    Serial.print( "Connected to Wifi with IP: " );
+    Serial.println(WiFi.localIP());
+  });
+
+  disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected & event)
+  {
+    // digitalWrite(LED_BUILTIN, LOW);  // BUILTIN LED on to indicate no WiFi connection
+    Serial.println( "Disconnected from WiFi" );
+    connectWifi();
+  });
 
   connectWifi();
 
-  Serial.println("Connected to wifi");
   ThingSpeak.begin(client);
 }
 
@@ -37,30 +82,8 @@ void loop()
 
     ThingSpeak.setField(1, dhtHumidity);
     ThingSpeak.setField(2, dhtTemp);
-    ThingSpeak.writeFields(channelID, writeAPIKey);
+    ThingSpeak.writeFields(tsChannelID, tsWriteAPIKey);
 
     lastConnectionTime = millis();
   }
-}
-
-// Attempts a connection to WiFi network and repeats until successful.
-int connectWifi()
-{
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("Connecting to WiFi");
-    delay(2500);
-  }
-  Serial.println("Connected");
-}
-
-// Get the most recent readings for temperature and humidity.
-void updateDHT()
-{
-  // To return Fahrenheit use dht.readTemperature(true)
-  dhtTemp = dht.readTemperature();
-  dhtHumidity = dht.readHumidity();
-  Serial.println(dhtTemp);
-  Serial.println(dhtHumidity);
 }
